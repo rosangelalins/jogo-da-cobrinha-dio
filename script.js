636a4f8cb9094273d432c8ffe1b11396564af4c2
rosangelalins <rosangela.mslins@gmail.com>
2021-05-10 12:43:43 -0300

    cria cobrinha

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ let context = canvas.getContext("2d"); //plano 2d
 let box = 32; //tamanho total dos quadradinhos no jogo
 let snake = []; //criar uma lista que será a cobrinha e que ela aumentará de tamanho 
                 //que quando pintadas, criam os quadradinhos
+                
+snake[0] ={
+x: 8 * box,
+ y: 8 * box
+ }          
 
 function criarBG(){
     //fillStyle trabalha o estilo do canvas
@@ -12,4 +17,13 @@ function criarBG(){
     context.fillRect(0, 0, 16*box, 16*box); //utiliza o tamanho do box que foi criado de 32px
 }
 
+function criarCobrinha(){
+for (i  = 0; i < snake.length; i++) {
+    context.fillStyle ="green";
+    context.fillRect(snake[i].x, snake[i].y, box, box);
+    
+}
+}
+
 criarBG(); // aqui chama a função para mostrar o jogo
+criarCobrinha();
